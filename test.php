d3440030afc6c31f44344123c97d8fe82572796f
vantrisimanjuntak <vantri_antonius@yahoo.co.id>
2019-08-05 09:27:14 +0700

    Commit kedua

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
 <?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
+<?php echo "Ini git pertama saya" ?>
